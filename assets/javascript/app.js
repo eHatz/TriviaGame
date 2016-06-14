ff1fdc35b74f88729d677bb3feef85896b245999
Efthemi Hatzis <ehatz01@gmail.com>
2016-06-14 14:55:39 -0400

    finished

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,4 @@
-//Albert Einstein, in his theory of special relativity, determined that the laws of physics are the same for 
-//all non-accelerating observers, and he showed that the speed of light within a vacuum is the same no matter 
-//the speed at which an observer travels. Based on this theory, if a tree falls in the woods, how many pancakes
-//does the bear have?
-
-// click on small craig
-
-// wait for it
-
 // click on the green button
-
-// click on jump scare
-
-//choose the best presidential candidate
-
-//what was the answer to question 2
 
 //click on the biggest troll
 
@@ -28,12 +13,14 @@ var allQuestions = {
 	time: 30,
 	startGame: function(){ // initial button
 		questNum = 0;
+
 		var startBtn = $('<button/>', {
 			text: 'Start!',
 			id: 'start',
 			class: 'btn btn-primary answerButtons'
 		});
-		$('.ansDiv').html(startBtn);
+		$('.questDiv').html(startBtn);
+		$('.ansDiv').html('<img id="frog" src="assets/images/frog.jpg" />');
 		$('#start').on('click', allQuestions.question1);
 	},
 	timer: function() {
@@ -52,7 +39,7 @@ var allQuestions = {
 	wrong: function() {
 		wrongCount++;
 		var wrongSound = $('<audio>', {
-			src: 'assets/images/wrong-sound.mp3',
+			src: 'assets/images/buzzer.mp3',
 			id: 'wrong-Sound',
 			type: 'audio/mpeg',
 		});
@@ -62,14 +49,14 @@ var allQuestions = {
 		clearInterval(rhetoricalTime);
 		$('.time').html('');
 		$('.questDiv').html('');
-		$('.ansDiv').html('<img id="redX" src="assets/images/Red-X.png" />');
-		$('#redX').delay(500).fadeOut();
-		setTimeout(allQuestions.check, 1000);
+		$('.ansDiv').html('<img id="redX" src="assets/images/wrong.gif" />');
+		$('#redX').delay(1000).fadeOut();
+		setTimeout(allQuestions.check, 1500);
 	},
 	right: function() {
 		rightCount++;
 		var rightSound = $('<audio>', {
-			src: 'assets/images/clap2.mp3',
+			src: 'assets/images/clap.mp3',
 			id: 'right-Sound',
 			type: 'audio/mpeg',
 		});
@@ -93,7 +80,8 @@ var allQuestions = {
 		else if (questNum === 6){allQuestions.question7();}
 		else if (questNum === 7){allQuestions.question8();}
 		else if (questNum === 8){allQuestions.question9();}
-		else if (questNum === 9){allQuestions.startGame();}		
+		else if (questNum === 9){allQuestions.question10();}
+		else if (questNum === 10){allQuestions.end();}		
 	},
 	question1: function() {
 		questNum++
@@ -151,10 +139,8 @@ var allQuestions = {
 		$('.ansDiv').append('<img id="peleg" src="assets/images/peleg.png" width=80px;/>');
 		$('.ansDiv').append('<img id="peleg" src="assets/images/peleg.png" width=200px;/>');
 		$('.ansDiv').append('<img id="peleg" src="assets/images/peleg.png" width=120px;/>');
-		$('.ansDiv').append('<img id="peleg" src="assets/images/peleg.png" width=75px;/>');
-		$('.ansDiv').append('<img id="peleg" src="assets/images/peleg.png" width=240px;/>');
-		$('.ansDiv').append('<img id="peleg" src="assets/images/peleg.png" width=190px;/>');
 		$('.ansDiv').append('<img id="peleg" src="assets/images/peleg.png" width=25px;/>');
+		$('.ansDiv').append('<img id="peleg" src="assets/images/peleg.png" width=240px;/>');
 		function bigPeleg() {
 			$('.ansDiv').html('<img id="bigPelegImg" src="assets/images/peleg.png" width=500px;/>');
 			$('#bigPelegImg').on('click', allQuestions.right);
@@ -212,24 +198,21 @@ var allQuestions = {
 	question6: function() {
 		questNum++
 		allQuestions.timer();
-		var q6Ans = $('<span>', {
-			text: 'Craig',
-			id: 'q6Answer'
+		$('.questDiv').html(questNum + '. What was the answer to question 2?');
+		var q8Answers1 = $('<button>', {
+			text: 'This One',
+			id: 'q8Wrong',
+			class: 'btn btn-primary answerButtons'
 		});
-
-		$('.questDiv').html(questNum + '. Click On The Smallest ');
-		$('.questDiv').append(q6Ans);
-		$('.ansDiv').html('<img id="craig" src="assets/images/craig.png" width=120px;/>');
-		$('.ansDiv').append('<img id="craig" src="assets/images/craig.png" width=150px;/>');
-		$('.ansDiv').append('<img id="craig" src="assets/images/craig.png" width=200px;/>');
-		$('.ansDiv').append('<img id="craig" src="assets/images/craig.png" width=90px;/>');
-		$('.ansDiv').append('<img id="craig" src="assets/images/craig.png" width=250px;/>');
-		$('.ansDiv').append('<img id="craig" src="assets/images/craig.png" width=190px;/>');
-		$('.ansDiv').append('<img id="craig" src="assets/images/craig.png" width=150px;/>');
-		$('.ansDiv').append('<img id="craig" src="assets/images/craig.png" width=275px;/>');
-		
-		$('.ansDiv #craig').on('click', allQuestions.wrong);
-		$('#q6Answer').on('click', allQuestions.right);
+		var q8Answers2 = $('<button>', {
+			text: 'This One',
+			id: 'q8Answer',
+			class: 'btn btn-primary answerButtons'
+		});
+		$('.ansDiv').html(q8Answers1);
+		$('.ansDiv').append(q8Answers2);
+		$('#q8Wrong').on('click', allQuestions.wrong);
+		$('#q8Answer').on('click', allQuestions.right);
 	},
 	question7: function() {
 		questNum++
@@ -262,21 +245,23 @@ var allQuestions = {
 	question8: function() {
 		questNum++
 		allQuestions.timer();
-		$('.questDiv').html(questNum + '. What was the answer to question 2?');
-		var q8Answers1 = $('<button>', {
-			text: 'This One',
-			id: 'q8Wrong',
-			class: 'btn btn-primary answerButtons'
+		var q6Ans = $('<span>', {
+			text: 'Craig',
+			id: 'q6Answer'
 		});
-		var q8Answers2 = $('<button>', {
-			text: 'This One',
-			id: 'q8Answer',
-			class: 'btn btn-primary answerButtons'
-		});
-		$('.ansDiv').html(q8Answers1);
-		$('.ansDiv').append(q8Answers2);
-		$('#q8Wrong').on('click', allQuestions.wrong);
-		$('#q8Answer').on('click', allQuestions.right);
+
+		$('.questDiv').html(questNum + '. Click On The Smallest ');
+		$('.questDiv').append(q6Ans);
+		$('.ansDiv').html('<img id="craig" src="assets/images/craig.png" width=120px;/>');
+		$('.ansDiv').append('<img id="craig" src="assets/images/craig.png" width=150px;/>');
+		$('.ansDiv').append('<img id="craig" src="assets/images/craig.png" width=200px;/>');
+		$('.ansDiv').append('<img id="craig" src="assets/images/craig.png" width=90px;/>');
+		$('.ansDiv').append('<img id="craig" src="assets/images/craig.png" width=250px;/>');
+		$('.ansDiv').append('<img id="craig" src="assets/images/craig.png" width=190px;/>');
+
+		
+		$('.ansDiv #craig').on('click', allQuestions.wrong);
+		$('#q6Answer').on('click', allQuestions.right);
 	},
 	question9: function() {
 		questNum++
@@ -290,7 +275,7 @@ var allQuestions = {
 			class: 'btn btn-primary answerButtons'
 		});
 		var q9Answers2 = $('<button>', {
-			text: 'Silence',
+			text: 'A black hole',
 			id: 'q9Wrong2',
 			class: 'btn btn-primary answerButtons'
 		});
@@ -307,9 +292,57 @@ var allQuestions = {
 		$('#q9Wrong3').on('click', allQuestions.wrong);
 		rhetoricalTime = setTimeout(allQuestions.right, 29000);
 	},
+	question10: function() {
+		questNum++
+		allQuestions.timer();
+		$('.questDiv').html(questNum + '. Rate this quiz');
+		var q10wrong = $('<button>', {
+			text: '10 / 10',
+			id: 'q10wrongAns1',
+			class: 'btn btn-primary answerButtons'
+		});
+		var q10correct = $('<button>', {
+			text: 'Would Bang',
+			id: 'q10answer',
+			class: 'btn btn-primary answerButtons'
+		});
+		var q10wrong2 = $('<button>', {
+			text: 'Meh...',
+			id: 'q10wrongAns2',
+			class: 'btn btn-primary answerButtons'
+		});		
+		$('.ansDiv').html(q10wrong);
+		$('.ansDiv').append(q10correct);
+		$('.ansDiv').append(q10wrong2);
+
+		$('#q10answer').on('click', allQuestions.right);
+		$('#q10wrongAns').on('click', allQuestions.wrong);
+		$('#q10wrongAns2').on('click', allQuestions.wrong);
+	},
+	end: function() {
+		if (rightCount <= 3 ) {
+			$('.questDiv').html('WOW you\'re pretty bad at this... <br> You got ' + rightCount + ' out of 10 right.');
+
+		} else if (rightCount > 3 && rightCount <= 7){
+			$('.questDiv').html('Not bad, not bad... <br> You got ' + rightCount + ' out of 10 right.');
+
+		} else if (rightCount > 7 && rightCount <=9 ){
+			$('.questDiv').html('You\'re retty good at this <br> You got ' + rightCount + ' out of 10 right.');
+
+		} else {
+			$('.questDiv').html('How many times have you done this quiz?? <br> You got ' + rightCount + ' out of 10 right.');
+		}
+		var resetBtn = $('<button>', {
+			text: 'Play Again',
+			id: 'playAgain',
+			class: 'btn btn-primary answerButtons'
+		});
+		$('.ansDiv').html(resetBtn);
+		$('#playAgain').on('click', allQuestions.startGame);	
+	}
 };
 allQuestions.startGame();
 // whats red and bad for your teeth? a brick
-//what do a banana and a hellicopter have in common? neither are a police officer
-//what do get when you cross a joke with a rhetorical question?
 //whats red and smells like blue paint? red paint
+
+// scrolll allllll thw way down
